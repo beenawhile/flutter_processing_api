08e656627cb556af58f74e1d2ba129a0c5e02d8c
beenawhile <beenawhile@users.noreply.github.com>
2022-03-09 23:31:06 +0900

    35: circle packing

---
+++ example/lib/coding_challenge/5_circle_packing/circle_packing_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CirclePackingScreen extends StatefulWidget {
+  const CirclePackingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CirclePackingScreen> createState() => _CirclePackingScreenState();
+}
+
+class _CirclePackingScreenState extends State<CirclePackingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
